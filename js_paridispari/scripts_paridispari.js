97fd237269ec43ba2d11330fbdb6287f146e735a
CappelliLeonardo <l.cappelli1995@gmail.com>
2023-12-13 11:44:40 +0100

    esercizio completato

--- js_paridispari/scripts_paridispari.js
+++ js_paridispari/scripts_paridispari.js
@@ -14,38 +14,38 @@
 
 
 
-const button = document.getElementById('button');
+// const button = document.getElementById('button');
 
-button.addEventListener("click" , function (){
-    let numeroRandomPc = Math.floor (Math.random() * 5)+1;
+// button.addEventListener("click" , function (){
+//     let numeroRandomPc = Math.floor (Math.random() * 5)+1;
 
-    console.log("Numero del pc ", numeroRandomPc);
-    numero_pc.innerHTML = ' il computer ha ottenuto un punteggio di:' + numeroRandomPc;
+//     console.log("Numero del pc ", numeroRandomPc);
+//     numero_pc.innerHTML = ' il computer ha ottenuto un punteggio di:' + numeroRandomPc;
 
-    const pariDispari = document.getElementById('sceltauno').value;
-    console.log ("è stato scelto", pariDispari, typeof pariDispari);
+//     const pariDispari = document.getElementById('sceltauno').value;
+//     console.log ("è stato scelto", pariDispari, typeof pariDispari);
 
-    const numeroSceltoUser = parseInt(document.getElementById('sceltadue').value);
-    console.log ("questa è la scelta del numero", numeroSceltoUser, typeof numeroSceltoUser);
+//     const numeroSceltoUser = parseInt(document.getElementById('sceltadue').value);
+//     console.log ("questa è la scelta del numero", numeroSceltoUser, typeof numeroSceltoUser);
 
-    let risultato = pari (numeroRandomPc + numeroSceltoUser);
-    console.log(risultato);
+//     let risultato = pari (numeroRandomPc + numeroSceltoUser);
+//     console.log(risultato);
 
-});
-
-
+// });
 
 
-function pari (numero) {
-    if ( (numero) % 2 == 0 ){
-        return ('pari')
 
-    }
-    else{
-        return ('dispari')
-    }
 
-};
+// function pari (numero) {
+//     if ( (numero) % 2 == 0 ){
+//         return ('pari')
+
+//     }
+//     else{
+//         return ('dispari')
+//     }
+
+// };
 
 
 
@@ -54,3 +54,42 @@ function pari (numero) {
 // stabiliamo se somma numeri è pari o dispari tramite la funzione 
 
 // dichiarare vincitore
+
+
+const userChoice = prompt('scegli pari o dispari');
+console.log('userChoice', userChoice, typeof userChoice);
+
+const userNumber = parseInt(prompt('scegli un numero tra 1 e 5:'));
+console.log('userNumber', userNumber, typeof userNumber);
+
+const pcNumber = generateRandomNumber(1, 5);
+console.log('pcNumber', pcNumber, typeof pcNumber);
+
+const sum = userNumber + pcNumber;
+console.log ('sum', sum, typeof sum);
+
+const result = isEven(sum);
+console.log ( 'result', result, typeof result);
+
+if (result && userChoice == 'pari') {
+    alert ('Hai vinto');
+}
+else{
+    alert ('hai perso');
+}
+
+
+// FUNZIONI
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isEven(n){
+    if (n % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
